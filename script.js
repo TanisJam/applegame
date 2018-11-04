e51fda55980f872888d9144b7d49f6a9b1462cf6
TanisJam <mauricionromero@hotmail.com>
2018-11-03 22:07:08 -0300

    Icon for trees and script change

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const width = 10;
 const height = 10; 
 
 let createBoard = function(width, height, element){
+    //create nested divs
     for(let x = 0; x < height; x++){
         let row = document.createElement('div');
         row.id = "row-"+(x+1);  
@@ -14,24 +15,30 @@ let createBoard = function(width, height, element){
         }
         element.appendChild(row);
     }
-    return element;
+    //create a 2d array with all divs
+    let field = [];
+    for(let i = 0; i < height; i++){
+        field.push(element.childNodes[i].childNodes)
+    }
+    //return it
+    return field;
 }
 
-//create board and getting references on field array
+//create board, a matrix with all the divs
 let board = createBoard(width, height, document.getElementById("board"));
-let field = [];
-for(let i = 0; i < height; i++){
-    field.push(board.childNodes[i].childNodes)
-}
-console.log(field);
 
-for(let x = 0; x < field.length; x++){
-    for(let y = 0; y < field[x].length; y++){
+//console.log(board);
+
+for(let x = 0; x < board.length; x++){
+    for(let y = 0; y < board[x].length; y++){
         if(Math.random() > 0.9){
-            //let icon = document.createElement('i');
-            //field[x][y].appendChild(icon);
-        field[x][y].style.background = '#da1e1e';
-        field[x][y].style.borderRadius = '50%';
+            //board[x][y].style.background = '#da1e1e';
+            //board[x][y].style.borderRadius = '50%';
+            let icon = document.createElement('i');
+            icon.classList.add("fas");
+            icon.classList.add("fa-tree");
+            icon.style.color = 'green';
+            board[x][y].appendChild(icon);
         }
     }
 }
